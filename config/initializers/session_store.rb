9747bb3700a1cf4506690b2897b66fd9d77e4d57
Kath <kathf@hotmail.com>
2015-07-06 10:08:08 +1000

    add readme

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_maptest_session'
+Rails.application.config.session_store :cookie_store, key: '_waytogo2_session'

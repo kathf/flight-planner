ca4245806130aee2409e6f81622c5c57bef2d58f
Kath <you@example.com>
2015-07-17 09:28:42 +1000

    remove pg earth distance extensions

--- db/schema.rb
+++ db/schema.rb
@@ -15,8 +15,6 @@ ActiveRecord::Schema.define(version: 20150709025754) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-  enable_extension "cube"
-  enable_extension "earthdistance"
 
   create_table "airports", force: :cascade do |t|
     t.string   "fs"
